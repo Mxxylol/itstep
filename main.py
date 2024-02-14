3364d7d725fab6e14492bf7e4f6851e6ebe48ffa
Mxxylol <mxxylol@gmail.com>
2024-02-14 17:41:09 +0200

    First commit

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print("Hello")
-print("Hello")
-print("Hello")
-input()
+class Student:
+    print("Hi")
+
+    def __init__(self, height = 160):
+        self.height = height
+
+nick = Student()
+kate = Student(height=160)
+bbb = Student(height=99)
+print(nick.height)
+print(kate.height)
+print(bbb.height)
